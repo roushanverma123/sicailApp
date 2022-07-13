c027e716b4e1a2e66a61c0df8b92f456d10885e0
Roushan Verma <roushanverma123@gmail.com>
2022-07-13 22:52:49 +0530

    added linkedin and facebook routes

--- index.js
+++ index.js
@@ -19,11 +19,31 @@ app.get('/api/v1/instagram', (req, res) => {
     following:77,
     date: Date.now(),
   }
-  res.status(200).send({instaSocial});
+  res.status(200).json(instaSocial);
 
 });
 
+app.get('/api/v1/facebook', (req, res) => {
+  const facebookSocial = {
+    username: "roushanvermapage",
+    followers:100,
+    following:200,
+    date: Date.now(),
+  }
+  res.status(200).json(facebookSocial);
 
+});
+
+app.get('/api/v1/linkedin', (req, res) => {
+  const linkedinSocial = {
+    username: "roushanvermaofficial",
+    followers:66,
+    following:77,
+    date: Date.now(),
+  }
+  res.status(200).json(linkedinSocial);
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
