2302aac7f01986a9d88704aa8e7d4806ccded42a
Roushan Verma <roushanverma123@gmail.com>
2022-07-13 22:37:33 +0530

    Added Greetings route

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! From Roushan!!')
-})
+  res.send('Hello World! From Roushan!!');
+
+});
+
+app.get('/greetings', (req, res) => {
+  res.send('Greetings! From Roushan!!');
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
